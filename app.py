80815b820f1fe5b5ee5a90a9eaa1070753eaa80c
muhammadfathoni-60 <muhammadfathoni432@gmail.com>
2025-06-29 14:35:24 +0000

    halo

--- app.py
+++ app.py
@@ -3,10 +3,7 @@ from src import app
 
 if __name__ == "__main__":
     try:
-        app.secret_key = os.urandom(12)
-        app.run(debug=True, host="0.0.0.0", port=9000)
-    except Exception as e:
-        print(f"Error: {e}")
-
-    except Exception as e:
-        print(e)
+        app.secret_key = os.urandom(12)  # Generate a random secret key
+        app.run(debug=True, host="0.0.0.0", port=9000)  # Run the Flask app
+    except Exception as err:
+        print(f"Terjadi kesalahan: {err}")
