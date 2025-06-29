d606c1ca4eb4c957b2050415864455008a0d1168
muhammadfathoni-60 <muhammadfathoni432@gmail.com>
2025-06-29 13:43:45 +0000

    class diagram

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ from src import app
 if __name__ == "__main__":
     try:
         app.secret_key = os.urandom(12)
-        app.run(debug=True, port=9000)
+        app.run(debug=True, host="0.0.0.0", port=9000)
+    except Exception as e:
+        print(f"Error: {e}")
+
     except Exception as e:
         print(e)
